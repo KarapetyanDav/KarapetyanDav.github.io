99e586d80d069fd3e683856f76f843543155d710
Davit <111378759+KarapetyanDav@users.noreply.github.com>
2023-04-09 15:54:15 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,96 @@
+function svgmenu1() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+
+    if (menujs.style.opacity === "0") {
+        menujs.style.opacity = "100%";
+        menujs.style.transform = "translateX(0)";
+        blureffect.style.opacity = "100%";
+        svg1.style.transform = "translateY(8px) rotate(45deg)";
+        svg2.style.transform = "translateY(1px) rotate(-45deg)";
+
+    } else {
+        menujs.style.transform = "translateX(200px)";
+        menujs.style.opacity = "0";
+        blureffect.style.opacity = "0";
+        svg1.style.transform = "translateY(0px) rotate(0deg)";
+        svg2.style.transform = "translateY(0px) rotate(0deg)";
+    }
+}
+
+function closemenu() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+    menujs.style.transform = "translateX(200px)";
+    menujs.style.opacity = "0";
+    blureffect.style.opacity = "0";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
+function closemenutext() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+    menujs.style.transform = "translateX(200px)";
+    menujs.style.opacity = "0";
+    blureffect.style.opacity = "0";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
+function closemenutext2() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+    menujs.style.transform = "translateX(200px)";
+    menujs.style.opacity = "0";
+    blureffect.style.opacity = "0";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
+function closemenutext3() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+    menujs.style.transform = "translateX(200px)";
+    menujs.style.opacity = "0";
+    blureffect.style.opacity = "0";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
+function closemenutext4() {
+    var menujs = document.getElementById('rightmenujs');
+    var blureffect = document.getElementById('blureffectjs');
+    var svg1 = document.getElementById('svg01');
+    var svg2 = document.getElementById('svg02');
+    menujs.style.transform = "translateX(200px)";
+    menujs.style.opacity = "0";
+    blureffect.style.opacity = "0";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
