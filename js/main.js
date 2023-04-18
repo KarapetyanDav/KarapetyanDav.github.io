6e34a8cfab99111cb17fd2369b274bc31e39d2c2
Davit <111378759+KarapetyanDav@users.noreply.github.com>
2023-04-18 21:32:43 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+function closemenu() {
+    const menujs = document.getElementById('rightmenujs');
+    const blureffect = document.getElementById('blureffectjs');
+    const svg1 = document.getElementById('svg01');
+    const svg2 = document.getElementById('svg02');
+
+    menujs.style.transform = "translateX(100%)";
+    blureffect.style.transform = "translateX(-100%)";
+    svg1.style.transform = "translateY(0px) rotate(0deg)";
+    svg2.style.transform = "translateY(0px) rotate(0deg)";
+}
+
 function openmenu() {
     const menujs = document.getElementById('rightmenujs');
     const blureffect = document.getElementById('blureffectjs');
@@ -11,34 +23,6 @@ function openmenu() {
         svg2.style.transform = "translateY(1px) rotate(-45deg)";
 
     } else {
-        menujs.style.transform = "translateX(100%)";
-        blureffect.style.transform = "translateX(-100%)";
-        svg1.style.transform = "translateY(0px) rotate(0deg)";
-        svg2.style.transform = "translateY(0px) rotate(0deg)";
+       closemenu()
     }
 }
-
-function closemenu() {
-    const menujs = document.getElementById('rightmenujs');
-    const blureffect = document.getElementById('blureffectjs');
-    const svg1 = document.getElementById('svg01');
-    const svg2 = document.getElementById('svg02');
-    menujs.style.transform = "translateX(100%)";
-    blureffect.style.transform = "translateX(-100%)";
-    svg1.style.transform = "translateY(0px) rotate(0deg)";
-    svg2.style.transform = "translateY(0px) rotate(0deg)";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
